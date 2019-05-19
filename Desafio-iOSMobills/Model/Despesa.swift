f11126d30d285fcfd52f42097644ff036d4fab55
JoaoFelipeMaciel <joaofelipemaciel@gmail.com>
2019-05-19 10:52:11 -0300

    initial commit

---
+++ Desafio-iOSMobills/Model/Despesa.swift
@@ -0,0 +1,13 @@
+//
+//  Despesa.swift
+//  Desafio-iOSMobills
+//
+//  Created by João Felipe Maciel on 19/05/19.
+//  Copyright © 2019 João Felipe Maciel. All rights reserved.
+//
+
+import UIKit
+
+class Despesa: NSObject {
+
+}
